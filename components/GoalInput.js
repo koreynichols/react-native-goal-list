0902a80e5b68dd3a2dd3f5f131cb1817aa65cea1
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2020-02-20 13:02:32 -0800

    created goal list app in react native

---
+++ components/GoalInput.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import {View, TextInput, Button, StyleSheet, Modal} from 'react-native';
+
+const GoalInput = props => {
+    const [enteredGoal, setEnteredGoal] = useState('');
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText)
+    };
+
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+    }
+    
+    return (
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+            <TextInput 
+                placeholder="Course Goals" 
+                style={styles.input}
+                onChangeText={goalInputHandler}
+                value={enteredGoal}
+            />
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <Button title="CANCEL" color="red" onPress={props.onCancel} />
+                </View>
+                <View style={styles.button}>
+                    <Button title="ADD" onPress={addGoalHandler} />
+                </View>
+            </View>
+        </View>
+      </Modal>
+    )
+};
+
+const styles = StyleSheet.create({
+    inputContainer: { 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        flex: 1
+    },
+    input: {
+        borderBottomColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        width: '80%',
+        marginBottom: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '60%'
+    },
+    button: {
+        width: '40%'
+    }
+});
+
+export default GoalInput;
